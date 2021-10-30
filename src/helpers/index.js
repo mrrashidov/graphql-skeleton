81d363022a4e3543870507e6d96e872e75b127f7
mrrashidov <dev.rashidov@gmail.com>
2021-10-30 17:45:05 +0300

    Created date scalar

---
+++ src/helpers/index.js
@@ -0,0 +1,8 @@
+const doesPathExist = require('./does-path-exists'),
+	formatDate = require('./format-date'),
+	validateInput = require('./validate-input')
+module.exports = {
+	validateInput,
+	doesPathExist,
+	formatDate
+}
